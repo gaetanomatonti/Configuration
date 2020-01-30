6ead77b6b68215ec721adae7cdc1901049a094fa
g.matonti <g.matonti@reply.it>
2020-01-30 16:19:05 +0100

    Added Configuration

--- Sources/Configuration/Configuration.swift
+++ Sources/Configuration/Configuration.swift
@@ -1,3 +1,30 @@
-struct Configuration {
-    var text = "Hello, World!"
+import Foundation
+
+public enum Configuration {
+    enum Error: Swift.Error, Equatable, LocalizedError {
+        case missingKey, invalidValue
+        
+        var errorDescription: String? {
+            switch self {
+            case .missingKey: return "The key is missing from the config file."
+            case .invalidValue: return "The decoded value is of the wrong type."
+            }
+        }
+    }
+    
+    static func value<T>(forKey key: String) throws -> T where T: LosslessStringConvertible {
+        guard let object = Bundle.main.object(forInfoDictionaryKey: key) else { throw Error.missingKey }
+        
+        switch object {
+        case let value as T:
+            return value
+        case let string as String:
+            guard let value = T(string) else { fallthrough }
+            return value
+        default:
+            throw Error.invalidValue
+        }
+    }
+    
 }
+
